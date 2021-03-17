adfe7334fafc6398d80650b9eaceeebf63e6db20
Sujackson <su_jackson99@yahoo.com>
2021-03-18 02:16:44 +0700

    setting db

--- db/index.js
+++ db/index.js
@@ -1,9 +1,18 @@
 const mysql = require("mysql");
+
+//mysql://bd1eadff1c5fec:692886c2@us-cdbr-east-03.cleardb.com/heroku_2205579f265d37e?reconnect=true
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'tjs'
+// });
+
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'tjs'
+    host: 'us-cdbr-east-03.cleardb.com',
+    user: 'bd1eadff1c5fec',
+    password: '692886c2',
+    database: 'heroku_2205579f265d37e'
 });
 
 db.connect((err)=>{
